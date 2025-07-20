a264ced8ede9f532fd23d6f85bcbdbdba1412529
= <aalna096@uottawa.ca>
2025-07-20 15:04:37 -0400

    fixd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ lang, setLang }) => {
   const t = {
     en: {
-      title: "SmartSpend",
+      title: "SpendSmart",
       overview: "Overview",
     },
     fr: {
